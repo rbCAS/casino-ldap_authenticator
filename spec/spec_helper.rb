1750643252a2d04e593d5812d90b07a476f7a8ac
Nils Caspar <ncaspar@me.com>
2013-01-01 13:51:24 +0100

    SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec'
+end
+
 require 'rspec'
 require 'casino_core-authenticator-ldap'
 
@@ -8,5 +14,9 @@ require 'casino_core-authenticator-ldap'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = 'random'
 end

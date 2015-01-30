f6e7bb3ae8599e95fe7bfc6834dffd052922d511
Nils Caspar <ncaspar@me.com>
2015-01-30 11:56:09 +0100

    Make it threadsafe

--- lib/casino/ldap_authenticator.rb
+++ lib/casino/ldap_authenticator.rb
@@ -10,16 +10,12 @@ class CASino::LDAPAuthenticator
   end
 
   def validate(username, password)
-    @username = username
-    @password = password
     begin
-      connect
-      authenticate
-      if !@user_plain
+      user_plain = authenticate(username, password)
+      if !user_plain
         false
       else
-        generate_user
-        @user
+        generate_user(user_plain)
       end
     rescue Net::LDAP::LdapError => e
       raise CASino::Authenticator::AuthenticatorError,
@@ -28,53 +24,56 @@ class CASino::LDAPAuthenticator
   end
 
   private
-  def connect
+  def ldap
+    return @ldap if @ldap
     @ldap = Net::LDAP.new
     @ldap.host = @options[:host]
     @ldap.port = @options[:port]
     if @options[:encryption]
       @ldap.encryption(@options[:encryption].to_sym)
     end
+    return @ldap
   end
 
-  def authenticate
+  def authenticate(username, password)
     unless @options[:admin_user].nil?
-      @ldap.auth(@options[:admin_user], @options[:admin_password])
+      ldap.auth(@options[:admin_user], @options[:admin_password])
     end
-    @user_plain = @ldap.bind_as(:base => @options[:base], :size => 1, :password => @password, :filter => user_filter)
-    if @user_plain
+    user_plain = ldap.bind_as(:base => @options[:base], :size => 1, :password => password, :filter => user_filter(username))
+    if user_plain
       include_attributes = @options[:extra_attributes].values + [username_attribute]
-      @user_plain = @ldap.search(:base => @options[:base], :filter => user_filter, :attributes => include_attributes)
-      if @user_plain.is_a?(Array)
-        @user_plain = @user_plain.first
+      user_plain = ldap.search(:base => @options[:base], :filter => user_filter(username), :attributes => include_attributes)
+      if user_plain.is_a?(Array)
+        user_plain = user_plain.first
       end
     end
+    return user_plain
   end
 
   def username_attribute
     @options[:username_attribute] || DEFAULT_USERNAME_ATTRIBUTE
   end
 
-  def user_filter
-    filter = Net::LDAP::Filter.eq(username_attribute, @username)
+  def user_filter(username)
+    filter = Net::LDAP::Filter.eq(username_attribute, username)
     unless @options[:filter].nil?
       filter &= Net::LDAP::Filter.construct(@options[:filter])
     end
     filter
   end
 
-  def generate_user
-    @user = {
-      username: @user_plain[username_attribute].first,
-      extra_attributes: extra_attributes
+  def generate_user(user_plain)
+    {
+      username: user_plain[username_attribute].first,
+      extra_attributes: extra_attributes(user_plain)
     }
   end
 
-  def extra_attributes
+  def extra_attributes(user_plain)
     if @options[:extra_attributes]
       result = {}
       @options[:extra_attributes].each do |index_result, index_ldap|
-        value = @user_plain[index_ldap]
+        value = user_plain[index_ldap]
         if value
           result[index_result] = "#{value.first}"
         end
